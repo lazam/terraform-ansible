1835168976522d4fe4d83ed112797d5594fe68d2
lazam <aws.lazamanthony@gmail.com>
2016-04-23 11:59:40 +0800

    Rectify some configurations

--- main.tf
+++ main.tf
@@ -21,6 +21,23 @@ resource "aws_instance" "main" {
         key_file = "${var.key_path}"
         agent = "false"
     }
+    
+    provisioner "file" {
+        source = "files/"
+        destination = "/tmp"
+    }
+
+     provisioner "file" {
+        source = "playbook/"
+        destination = "/tmp"
+    }
+
+
+      provisioner "file" {
+        source = "${var.key_path}"
+        destination = "/home/ubuntu/.ssh/SSHkey"
+    }
+
 
     /* Install and configure Ansible */
 
@@ -31,7 +48,13 @@ resource "aws_instance" "main" {
             "sudo pip install boto",
             "cd /etc/ansible/ ; sudo wget -O ec2.py https://raw.githubusercontent.com/ansible/ansible/devel/contrib/inventory/ec2.py",
             "cd /etc/ansible/ ; sudo wget -O ec2.ini https://raw.githubusercontent.com/ansible/ansible/devel/contrib/inventory/ec2.ini",
+            "sudo chmod 777 /etc/ansible/ec2.py",
+            "sudo mkdir /etc/ansible/playbooks && sudo mkdir /etc/ansible/roles",
+            "sudo rm -rf /etc/ansible/ansible.cfg && sudo mv /tmp/ansible.cfg /etc/ansible/",
+            "mv /tmp/config ~/.ssh/config && chmod 600 ~/.ssh/config && chmod 400 ~/.ssh/SSHkey",
+            "sudo mv /tmp/*.yaml /etc/ansible/playbooks/",
+
         ]
-    } 
+    }
 
 }
